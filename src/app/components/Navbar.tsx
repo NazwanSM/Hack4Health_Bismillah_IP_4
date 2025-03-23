e4d941af4079e1e137170e9e4d74c82cf5107918
Muhamad Hasbullah Faris <18223014@std.stei.itb.ac.id>
2025-03-23 23:11:00 +0700

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,6 @@ return (
     <div className="fixed bottom-0 left-0 right-0 max-w-md mx-auto bg-[#fffdf5] border-t border-zinc-200">
         
         <div className="flex justify-between items-end px-6 pb-2">
-            {/* Home */}
             <Link href="/" className="flex flex-col items-center gap-1 w-12">
             <img 
                 src={pathname === '/' ? "/icon/home.svg" : "/icon/not-home.svg"}
@@ -21,8 +20,6 @@ return (
                 Beranda
             </span>
             </Link>
-
-            {/* Article */}
             <Link href="/artikel" className="flex flex-col items-center gap-1 w-14">
             <img 
                 src={pathname === '/artikel' ? "/icon/article.svg" : "/icon/not-article.svg"}
@@ -32,8 +29,6 @@ return (
                 Artikel
             </span>
             </Link>
-
-            {/* Emergency */}
             <Link href="/darurat" className="flex flex-col items-center relative w-14">
             <div className="w-12 h-12 bg-[#364C84] rounded-full flex items-center justify-center mb-1">
                 <div className="relative">
@@ -47,8 +42,6 @@ return (
                 Darurat
             </span>
             </Link>
-
-            {/* Services */}
             <Link href="/layanan" className="flex flex-col items-center gap-1 w-14">
             <img 
                 src={pathname === '/layanan' ? "/icon/layanan.svg" : "/icon/not-layanan.svg"}
@@ -58,8 +51,6 @@ return (
                 Layanan
             </span>
             </Link>
-
-            {/* Profile */}
             <Link href="/profil" className="flex flex-col items-center gap-1 w-14">
             <img 
                 src={pathname === '/profil' ? "/icon/profile.svg" : "/icon/not-profile.svg"}
